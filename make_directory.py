284eec4128db7b2379a7ceaf7125c9e52e437a82
pennycuda <141153875+pennycuda@users.noreply.github.com>
2026-03-06 15:33:30 -0500

    Update assets API usage

--- make_directory.py
+++ make_directory.py
@@ -1,129 +1,107 @@
 #!/usr/bin/env python3
-import json
 from argparse import ArgumentParser
-from os import fspath, walk
 from pathlib import Path
-from subprocess import check_call
 
 import pandas as pd
-from antibodies_tsv_util import find_antibodies_meta
+import re
+import requests
+import subprocess
 
 
-def find_files(directory, patterns):
-    for dirpath_str, dirnames, filenames in walk(directory):
-        dirpath = Path(dirpath_str)
-        for filename in filenames:
-            filepath = dirpath / filename
-            for pattern in patterns:
-                if filepath.match(pattern):
-                    return filepath
+def download_from_assets(uuid, ancestor_uuid, file_patterns, out_dir, bearer_token = None):
+    files_directory = out_dir / uuid
+    files_directory.mkdir(parents=True, exist_ok=True)
 
-
-def find_file_pairs(directory):
-    hdf5_pattern = ["out.hdf5"]
-
-    cell_count_pattern = [
-        "reg1_stitched_expressions.ome.tiff-cell_channel_total.csv",
-        "reg001_expr.ome.tiff-cell_channel_total.csv",
-    ]
-    adjacency_matrix_pattern = [
-        "reg1_stitched_expressions.ome.tiff_AdjacencyMatrix.mtx",
-        "reg001_expr.ome.tiff_AdjacencyMatrix.mtx",
-    ]
-    adjacency_matrix_labels_pattern = [
-        "reg1_stitched_expressions.ome.tiff_AdjacencyMatrixRowColLabels.txt",
-        "reg001_expr.ome.tiff_AdjacencyMatrixRowColLabels.txt",
-    ]
-    cell_centers_pattern = [
-        "reg1_stitched_expressions.ome.tiff-cell_centers.csv",
-        "reg001_expr.ome.tiff-cell_centers.csv",
-    ]
-
-    hdf5_file = find_files(directory, hdf5_pattern)
-    cell_count_file = find_files(directory, cell_count_pattern)
-    adjacency_matrix_file = find_files(directory, adjacency_matrix_pattern)
-    adjacency_matrix_labels_file = find_files(
-        directory, adjacency_matrix_labels_pattern
-    )
-    cell_centers_file = find_files(directory, cell_centers_pattern)
-
-    return (
-        hdf5_file,
-        cell_count_file,
-        adjacency_matrix_file,
-        adjacency_matrix_labels_file,
-        cell_centers_file,
-    )
-
-
-def get_input_directory(data_directory, uuid):
-    public_directory = data_directory / "public" / uuid
-    if public_directory.exists():
-        return public_directory
+    if bearer_token:
+        headers = {"Authorization": f"Bearer {bearer_token}"}
     else:
-        consortium_directory = data_directory / "consortium"
-        if consortium_directory.exists():
-            for subdir in consortium_directory.iterdir():
-                consortium_subdir = subdir / uuid
-                if consortium_subdir.exists():
-                    return consortium_subdir
+        headers = None
 
-
-def copy_file(file, files_directory):
-    check_call(
-        f"cp {fspath(file)} {files_directory}/{file.name}",
-        shell=True,
-    )
-
-
-def find_parent_file(input_directory, files_directory):
-    antibodies_file = find_antibodies_meta(input_directory)
-    if antibodies_file:
-        print(f"Antibodies TSV: {antibodies_file}")
-        copy_file(antibodies_file, files_directory)
-    else:
-        print(f"No antibodies TSV found in {input_directory}")
-
-
-def find_processed_files(uuids_df, files_base_directory, data_directory):
-    for _, row in uuids_df.iterrows():
-        uuid = row["uuid"]
-        immediate_descendant_ids = row["immediate_descendant_ids"]
-
-        files_directory = files_base_directory / uuid
-        files_directory.mkdir(parents=True, exist_ok=True)
-
-        input_directory = get_input_directory(data_directory, uuid)
-
-        if pd.notna(immediate_descendant_ids):  # If there are immediate descendants
-            find_parent_file(input_directory, files_directory)
-        else:
-            input_files = find_file_pairs(input_directory)
-            if input_files == (None, None, None, None, None):
-                print("No input files in: ", input_directory)
+    for pattern in file_patterns:
+        print(pattern)
+        out_file = files_directory / pattern
+        url = f"https://assets.api.sennetconsortium.org/{uuid}/sprm_outputs/{pattern}"
+        # Check if file exists and download
+        try:
+            head = requests.head(url, allow_redirects=True, headers=headers)
+            if head.status_code != 200:
+                print(f"{pattern} for {uuid} does not exist on server (status {head.status_code}).")
                 continue
-            print("Input directory:", input_directory)
-            print("Input files:", input_files)
-            for input_file in input_files:
-                copy_file(input_file, files_directory)
+        except requests.RequestException as e:
+            print(f"Error checking {uuid}: {e}")
+            continue
+        try:
+            print(f"Downloading {pattern} for {uuid}...")
+            with requests.get(url, stream=True, headers=headers) as r:
+                r.raise_for_status()
+                with open(out_file, "wb") as f:
+                    for chunk in r.iter_content(chunk_size=8192):
+                        f.write(chunk)
+            print(f"Downloaded {pattern} for {uuid} successfully.")
+        except requests.RequestException as e:
+            print(f"Failed to download {uuid}: {e}")
+
+    # TODO: Antibodies tsv time, need to figure out regex instead of hardcode
+    antibodies_url = f"https://assets.api.sennetconsortium.org/{ancestor_uuid}/extras/SenNet_antibodies_n40_060325.tsv"
+    out = Path(files_directory / "SenNet_antibodies_n40_060325.tsv")
+    try:
+        head = requests.head(antibodies_url, allow_redirects=True, headers=headers)
+        if head.status_code != 200:
+            print(f"Couldn't get antibodies for processed dataset {uuid}, might be filename issue.")
+            print(f"Tried path {antibodies_url}")
+    except requests.RequestException as e:
+        print(f"Error getting antibodies for processed dataset {uuid}: {e}")
+        print(f"Tried path {antibodies_url}")
+    try:
+        print(f"Found antibodies for processed dataset {uuid}, downloading {antibodies_url}...")
+        with requests.get(antibodies_url, stream=True, headers=headers) as r:
+            r.raise_for_status()
+            with open(out, "wb") as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+                print(f"Downloaded antibodies tsv successfully.")
+    except requests.RequestException as e:
+        print(f"Failed to download antibodies for processed dataset {uuid}: {e}")
 
 
-def main(data_directory: Path, uuids_file: Path, tissue: str):
-    uuids_df = pd.read_csv(uuids_file, sep="\t")
-    uuids_df = uuids_df.dropna(subset=["uuid"])  # Ensure UUIDs are valid
+# def find_antibodies_tsv(uuid, pattern, token = None):
+#     if token:
+#         headers = {"Authorization": f"Bearer {token}"}
+#     else:
+#         headers = None
+#     url = f"https://assets.api.sennetconsortium.org/{uuid}/{pattern}"
+#     response = requests.get(url, allow_redirects=True, stream=True, headers=headers)
 
+
+def main(uuids_file: Path, tissue: str, token = None):
+    df = pd.read_csv(uuids_file, sep = "\t")
+    processed_uuids = df["uuid"]
+    ancestor_uuids = df["ancestors"]
     files_base_directory = Path(f"{tissue}_files")
     files_base_directory.mkdir(exist_ok=True)
 
-    find_processed_files(uuids_df, files_base_directory, data_directory)
+    # SPRM outputs first, then antibodies tsv
+    hdf5_pattern = Path("out.hdf5")
+    cell_count_pattern = Path("aligned_tissue_0_expr.ome.tiff-cell_channel_total.csv")
+    adjacency_matrix_pattern = Path("aligned_tissue_0_expr.ome.tiff_AdjacencyMatrix.mtx")
+    adjacency_matrix_labels_pattern = Path("aligned_tissue_0_expr.ome.tiff_AdjacencyMatrixRowColLabels.txt")
+    cell_centers_pattern = Path("aligned_tissue_0_expr.ome.tiff-cell_centers.csv")
+    # antibodies_re = re.compile(r"^extras/.*antibodies.*\.tsv$")
+    file_patterns = [hdf5_pattern, cell_count_pattern, adjacency_matrix_pattern, adjacency_matrix_labels_pattern, cell_centers_pattern]
+    for uuid, ancestor in zip(processed_uuids, ancestor_uuids):
+        # TODO: need to figure out how to find the antibodies tsv with just a regex and not hardcode
+        # antibodies_pattern = find_antibodies_tsv(ancestor, antibodies_re, token)
+        # file_patterns.append("extras/SenNet_antibodies_n40_060325.tsv")
+        download_from_assets(uuid, ancestor, file_patterns, files_base_directory, token)
+
 
 
 if __name__ == "__main__":
     p = ArgumentParser()
-    p.add_argument("data_directory", type=Path)
     p.add_argument("uuids_file", type=Path)
     p.add_argument("tissue", type=str)
+    p.add_argument("bearer_token", type=str, nargs="?")
 
     args = p.parse_args()
 
-    main(args.data_directory, args.uuids_file, args.tissue)
+    main(args.uuids_file, args.tissue, args.bearer_token)
--- make_uuids_tsv.py
+++ make_uuids_tsv.py
@@ -7,40 +7,29 @@ import requests
 import yaml
 
 organ_types_yaml_file = "bin/organ_types.yaml"
+organ_uberon_file = "bin/organs.json"
 
-
-def get_uuids(organ_name_mapping: dict, organ: str):
-    organ_code_mapping = {organ_name_mapping[key]: key for key in organ_name_mapping}
-
-    must_conditions = [
-        {"match": {"dataset_type": "CODEX"}},
-        {"match": {"data_access_level": "public"}},
-    ]
-
-    if organ:
-        must_conditions.append(
-            {"match": {"origin_samples.organ": organ_code_mapping[organ]}}
-        )
-
-    query_payload = {
-        "from": 0,
-        "size": 10000,
-        "query": {
-            "bool": {
-                "must": must_conditions,
-                "must_not": [{"exists": {"field": "next_revision_uuid"}}],
-            }
-        },
+def get_uuids(organ_uberon: str, organism: str, bearer_token = None):
+    if bearer_token:
+        headers = {"Authorization": f"Bearer {bearer_token}"}
+    else:
+        headers = None
+    params = {
+        "status": "QA",
+        "dataset_type": "PhenoCycler [DeepCell + SPRM]",
     }
 
-    # Make the request
-    url = "https://search.api.hubmapconsortium.org/v3/search"
-    response = requests.post(url, json=query_payload)
+    if organ_uberon:
+        params["origin_samples.organ"] = organ_uberon
+
+    url = "https://search.api.sennetconsortium.org/param-search/datasets"
+    response = requests.get(url, params=params, headers=headers)
+    print("Request URL:", response.url)
     print("Response status code: ", response.status_code)
 
     # Handle a successful response
     if response.status_code == 200:
-        return process_response(response)
+        return process_response(response, organism)
 
     # Handle 303 redirection
     elif response.status_code == 303:
@@ -52,7 +41,7 @@ def get_uuids(organ_name_mapping: dict, organ: str):
         # Make a request to the redirection URL
         redirection_response = requests.get(redirection_url)
         if redirection_response.status_code == 200:
-            return process_response(redirection_response)
+            return process_response(redirection_response, organism)
 
     # Handle other error responses
     else:
@@ -60,52 +49,40 @@ def get_uuids(organ_name_mapping: dict, organ: str):
         return [], [], []
 
 
-def process_response(response):
+def process_response(response, organism):
     """
     Helper function to process the JSON response and extract UUIDs, HubMAP IDs, and donor metadata.
     """
     data = response.json()
-    hits = data.get("hits", {}).get("hits", [])
+    items = data
 
     uuids = []
-    hubmap_ids = []
+    sennet_ids = []
     donor_metadata_list = []
-    immediate_ancestor_uuids = []
-    immediate_descendant_uuids = []
-
-    # Loop through each dataset (hit) and extract the relevant information
-    for hit in hits:
-        source = hit["_source"]
-        uuids.append(source["uuid"])
-        hubmap_ids.append(source["hubmap_id"])
-        immediate_ancestor_uuids.append(
-            source["immediate_ancestor_ids"][0]
-            if source["immediate_ancestor_ids"]
-            else None
-        )
-
-        immediate_descendant = source.get("immediate_descendant_ids", [])
-        immediate_descendant_uuids.append(
-            None if not immediate_descendant else immediate_descendant
-        )
-
+    for item in items:
+        uuids.append(item.get("uuid"))
+        sennet_ids.append(item.get("sennet_id"))
+        sources = item.get("sources", [])
+        if not sources:
+            continue
+        source = sources[0]
+        if source.get("source_type").lower() == organism.lower():
         # Attempt to extract donor metadata, if available
-        donor_metadata = source.get("donor", {}).get("metadata", {})
-        donor_metadata_list.append(extract_donor_metadata(donor_metadata))
+            metadata = source.get("metadata")
+            if metadata.get("living_donor_data"):
+                donor_metadata = metadata.get("living_donor_data")
+            else:
+                donor_metadata = metadata.get("organ_donor_data")
+            donor_metadata_list.append(extract_donor_metadata(donor_metadata))
 
     return (
         uuids,
-        hubmap_ids,
+        sennet_ids,
         donor_metadata_list,
-        immediate_ancestor_uuids,
-        immediate_descendant_uuids,
     )
 
 
-def extract_donor_metadata(metadata):
-    """
-    Extract donor metadata, including age, sex, height, weight, and other relevant information.
-    """
+def extract_donor_metadata(donor_metadata):
     donor_info = {
         "age": None,
         "sex": None,
@@ -114,63 +91,100 @@ def extract_donor_metadata(metadata):
         "bmi": None,
         "cause_of_death": None,
         "race": None,
+        "medical_history": None,
+        "abo_blood_type": None,
+        "mechanism_of_injury": None,
     }
-
-    for item in metadata.get("organ_donor_data", []):
-        concept = item.get("grouping_concept_preferred_term")
-        value = item.get("data_value")
-
-        if concept == "Age":
-            donor_info["age"] = value
-        elif concept == "Sex":
-            donor_info["sex"] = item.get("preferred_term")
-        elif concept == "Height":
-            donor_info["height"] = value
-        elif concept == "Weight":
-            donor_info["weight"] = value
-        elif concept == "Body mass index":
-            donor_info["bmi"] = value
-        elif concept == "Cause of death":
-            donor_info["cause_of_death"] = item.get("preferred_term")
-        elif concept == "Race":
-            donor_info["race"] = item.get("preferred_term")
-
-    for item in metadata.get("living_donor_data", []):
-        concept = item.get("grouping_concept_preferred_term")
-        value = item.get("data_value")
-        if concept == "Age":
-            donor_info["age"] = value
-        elif concept == "Sex":
-            donor_info["sex"] = item.get("preferred_term")
-        elif concept == "Height":
-            donor_info["height"] = value
-        elif concept == "Weight":
-            donor_info["weight"] = value
-        elif concept == "Body mass index":
-            donor_info["bmi"] = value
-        elif concept == "Cause of death":
-            donor_info["cause_of_death"] = item.get("preferred_term")
-        elif concept == "Race":
-            donor_info["race"] = item.get("preferred_term")
-
+    for item in donor_metadata:
+        if item.get("grouping_concept_preferred_term") == "ABO blood group system":
+            donor_info["abo_blood_type"] = item.get("data_value")
+        elif item.get("grouping_concept_preferred_term") == "Age":
+            donor_info["age"] = item.get("data_value") + " " + item.get("units")
+        elif item.get("grouping_concept_preferred_term") == "Body Mass Index":
+            donor_info["bmi"] = item.get("data_value") + " " + item.get("units")
+        elif item.get("grouping_concept_preferred_term") == "Cause of Death":
+            donor_info["cause_of_death"] = item.get("data_value")
+        elif item.get("grouping_concept_preferred_term") == "Height":
+            donor_info["height"] = item.get("data_value") + " " + item.get("units")
+        elif item.get("grouping_concept_preferred_term") == "Mechanism of Injury":
+            donor_info["mechanism_of_injury"] = item.get("data_value")
+        elif item.get("grouping_concept_preferred_term") == "Race":
+            donor_info["race"] = item.get("data_value")
+        elif item.get("grouping_concept_preferred_term") == "Sex":
+            donor_info["sex"] = item.get("data_value")
+        elif item.get("grouping_concept_preferred_term") == "Medical History":
+            donor_info["medical_history"] = item.get("data_value")
+        elif item.get("grouping_concept_preferred_term") == "Weight":
+            donor_info["weight"] = item.get("data_value") + " " + item.get("units")
     return donor_info
 
 
-def main(tissue_type: str):
+def get_organ_uberon(organ_name):
+    term = organ_name.lower().strip()
+    with open(organ_uberon_file) as f:
+        data = json.load(f)
+    for entry in data:
+        if entry.get("term", "").lower() == term:
+            return entry["organ_uberon"]
+    for entry in data:
+        cat = entry.get("category")
+        if cat and cat.get("term", "").lower() == term:
+            return cat["organ_uberon"]
+    return None
+
+
+def get_ancestors(uuids, entity_type, bearer_token = None):
+    results = []
+    for uuid in uuids:
+        data = entity_api_request(f"/{entity_type}/{uuid}", bearer_token=bearer_token)
+        if not data:
+            results.append("NA")
+            continue
+        entry = data[0]
+        results.append(entry["uuid"])
+    return results
+
+
+def entity_api_request(endpoint, body = None, method = 'GET', bearer_token = None):
+    domain_base = "https://entity.api.sennetconsortium.org"
+    if bearer_token:
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {bearer_token}"
+        }
+    else:
+        headers = {"Content-Type": "application/json"}
+    query_url =  f"{domain_base}{endpoint}"
+    entity_data = None
+    try:
+        if method.lower() == 'post':
+            response = requests.post(query_url, data=body, headers=headers)
+        else:
+            response = requests.get(query_url, headers=headers)
+
+        response_code = response.status_code
+
+        if response_code == 200:
+            entity_data = response.json()
+        else:
+            print(f"An error occurred {response_code}")
+    except Exception as err:
+        print(f"An unexpected error occurred: {err}")
+
+    return entity_data
+
+
+def main(tissue_type: str, organism, bearer_token = None):
     organ_dict = yaml.load(open(organ_types_yaml_file), Loader=yaml.BaseLoader)
     for key in organ_dict:
         organ_dict[key] = organ_dict[key]["description"]
-    if tissue_type not in organ_dict.values():
-        print(f"Tissue {tissue_type} not found ")
-        tissue_type = None
-    uuids_list, hubmap_ids_list, donor_metadata, ancestor_uuids, descendant_uuids = (
-        get_uuids(organ_dict, tissue_type)
-    )
+    uberon_code = get_organ_uberon(tissue_type)
+    uuids_list, sennet_ids_list, donor_metadata = get_uuids(uberon_code, organism, bearer_token)
+    ancestors = get_ancestors(uuids_list, "ancestors", bearer_token)
     uuids_df = pd.DataFrame()
     uuids_df["uuid"] = pd.Series(uuids_list, dtype=str)
-    uuids_df["hubmap_id"] = pd.Series(hubmap_ids_list, dtype=str)
-    uuids_df["immediate_ancestor_ids"] = pd.Series(ancestor_uuids, dtype=str)
-    uuids_df["immediate_descendant_ids"] = pd.Series(descendant_uuids, dtype=str)
+    uuids_df["sennet_id"] = pd.Series(sennet_ids_list, dtype=str)
+    uuids_df["ancestors"] = pd.Series(ancestors, dtype=str)
     donor_metadata_df = pd.DataFrame(donor_metadata)
     result_df = pd.concat([uuids_df, donor_metadata_df], axis=1)
     key_for_tissue = [key for key, value in organ_dict.items() if value == tissue_type]
@@ -178,13 +192,14 @@ def main(tissue_type: str):
         output_file_name = f"{key_for_tissue[0].lower()}.tsv"
     else:
         output_file_name = "rna.tsv"
-    print(result_df)
     result_df.to_csv(output_file_name, sep="\t")
 
 
 if __name__ == "__main__":
     p = ArgumentParser()
     p.add_argument("tissue_type", type=str, nargs="?", help="Type of tissue (optional)")
+    p.add_argument("organism", type=str)
+    p.add_argument("bearer_token", type=str, nargs="?", help = "bearer token if accessing QA datasets")
     args = p.parse_args()
 
-    main(args.tissue_type)
+    main(args.tissue_type, args.organism, args.bearer_token)
