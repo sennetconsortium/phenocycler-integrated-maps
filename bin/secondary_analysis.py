f47b8248321f234a743dfa2af75cb002b77557b8
pennycuda <141153875+pennycuda@users.noreply.github.com>
2026-03-20 14:06:13 -0400

    back to reg scanpy

--- bin/secondary_analysis.py
+++ bin/secondary_analysis.py
@@ -51,9 +51,9 @@ def main(
     rsc.tl.umap(adata)
 
     # Move .X back to the CPU to use regular sc for leiden
-    # rsc.get.anndata_to_CPU(adata)
+    rsc.get.anndata_to_CPU(adata)
     # leiden clustering, maybe try the use dask param with rapids?
-    rsc.tl.leiden(adata, )
+    sc.tl.leiden(adata, )
 
     total_cell_count = adata.obs.shape[0]
     metadata = add_cell_counts(
@@ -62,8 +62,8 @@ def main(
     with open(f"{uuid}.json", "w") as outfile:
         json.dump(metadata, outfile)
 
-    # # Move .X back to the CPU to plot
-    rsc.get.anndata_to_CPU(adata)
+    # Move .X back to the CPU to plot
+    # rsc.get.anndata_to_CPU(adata)
 
     # Plot
     with plt.rc_context():
